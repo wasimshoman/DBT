760031b5247757ab7c2f9f04249695dbadce01a8
wasimshoman <wss7@hotmail.com>
2025-01-25 21:51:00 +0100

    add tests to the pipline

--- retail_pg/models/top_classes.sql
+++ retail_pg/models/top_classes.sql
@@ -1,16 +1,3 @@
 --Identifies the top 5 selling starship classes
-with class_details_cte as (
-select * from public.dim_starships as s
-inner join public.fact_starships as f
-on s.id	= f.id
-), 
-class_sums_cte as (
-select starship_class, count(*) as selling_count, SUM(cost_in_credits) as total_revenue
-from class_details_cte
-group by starship_class
-)
-select * from class_sums_cte
-where selling_count <> 0 and total_revenue <> 0
-order by selling_count desc
-
+SELECT * FROM {{ ref('class_details')}}
 limit 5
