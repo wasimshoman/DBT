db97b91b566a646982b47fe73185e82385ca60f3
wasimshoman <wss7@hotmail.com>
2025-01-28 09:30:17 +0100

    Update orchestrator.py

--- retail_pg/orchestrator.py
+++ retail_pg/orchestrator.py
@@ -7,61 +7,59 @@ the file then classsify the starship models with ML
 import subprocess
 import sys
 import os
-import logging
 
 class DataPipeline:
-    """manage the data pipeline"""
+    #Manage the data pipeline
 
     def __init__(self):
-        # enable logging
-        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+        # Set the script directory
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
 
     def run_python_script(self, script_name):
-        """Run a Python script"""
-        logging.info(f"Running {script_name}...")
+        #Python script
+        print(f"Running {script_name}...")
         try:
-            logging.info(f"Running {script_name} in: {self.script_dir}")
+            print(f"Running {script_name} in: {self.script_dir}")
             subprocess.run([sys.executable, script_name], check=True, cwd=self.script_dir)
-            logging.info(f"{script_name} executed successfully.")
+            print(f"{script_name} executed successfully.")
         except subprocess.CalledProcessError as e:
-            logging.error(f"Error running {script_name}: {e}")
-            sys.exit(1) #
+            print(f"Error running {script_name}: {e}")
+            sys.exit(1)
         except Exception as e:
-            logging.error(f"Unexpected error: {e}")
+            print(f"Unexpected error: {e}")
             sys.exit(1)
 
     def run_dbt_models(self):
-        """Run dbt models"""
-        logging.info("Running dbt models...")
+        #Run dbt models
+        print("Running dbt models...")
         try:
-            logging.info(f"Running dbt in: {self.script_dir}")
+            print(f"Running dbt in: {self.script_dir}")
             subprocess.run(["dbt", "run"], check=True, cwd=self.script_dir)
-            logging.info("dbt models executed successfully. Multiple views are created for visualization.")
+            print("dbt models executed successfully")
         except subprocess.CalledProcessError as e:
-            logging.error(f"Error running dbt models: {e}")
+            print(f"Error running dbt models: {e}")
             sys.exit(1)
         except NotADirectoryError as e:
-            logging.error(f"Invalid directory: {self.script_dir}")
+            print(f"Invalid directory: {self.script_dir}")
             sys.exit(1)
         except Exception as e:
-            logging.error(f"Unexpected error: {e}")
+            print(f"Unexpected error: {e}")
             sys.exit(1)
 
     def run_pipeline(self):
-        """Run the entire pipeline"""
+        #Run the entire pipeline
         try:
-            #Download and store data
+            #download and store data
             self.run_python_script("APIcaller.py")
 
             #Run dbt models
             self.run_dbt_models()
 
-            #Train the classifier
+            #Train the classifier (K means)
             self.run_python_script("Train_Classifier.py")
 
         except Exception as e:
-            logging.error(f"Pipeline failed: {e}")
+            print(f"Pipeline failed: {e}")
             sys.exit(1)
 
 if __name__ == "__main__":
